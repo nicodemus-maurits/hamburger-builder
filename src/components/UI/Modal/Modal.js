f81beae3c89efa4eaafc3c6146da1dace9d9b3e3
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-04-11 12:28:29 +0200

    Retrigger render method when show or childer props changed

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,11 +5,11 @@ import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
     shouldComponentUpdate (nextProps, nextState) {
-        return nextProps.show !== this.props.show;
-        /* is the same as */
-        /* if(nextProps.show !== this.props.show) {
+        //Retrigger render when show / children changed 
+        if(nextProps.show !== this.props.show || 
+            nextProps.children !== this.props.children) {
             return true;
-        } */
+        }
     }
 
     componentWillUpdate () {
