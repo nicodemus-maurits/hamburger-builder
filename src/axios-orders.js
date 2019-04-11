b433e8da9f232b803958e7f81ec1363cf87951c9
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-04-11 11:55:52 +0200

    Remove semicolon

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const instance = Axios.create({
-    baseURL: 'https://react-burgerbuilder-3f0bd.firebaseio.com/';
+    baseURL: 'https://react-burgerbuilder-3f0bd.firebaseio.com/'
 });
 
 export default instance;
