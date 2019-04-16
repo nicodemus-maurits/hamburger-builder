ba71537d95f07c0c1f7f50f94ce1a37b7516a96e
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-04-16 13:28:48 +0200

    Add onChange to bind the input element

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -80,6 +80,21 @@ class ContactData extends Component {
             .catch(error => this.setState({ loading: false }));
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        /* Clone original state orderForm key -> name, street... */
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        /* Clone original value from each key */
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        /* Set cloned value to event.target */
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({ orderForm: updatedOrderForm });
+    }
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -96,7 +111,8 @@ class ContactData extends Component {
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value} />
+                        value={formElement.config.value}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                 ))}
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
