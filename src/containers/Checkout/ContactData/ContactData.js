12c27e263275900659962dfa3f9f6c2a55fe726b
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-04-16 10:13:27 +0200

    Switch normal input to Iput component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
@@ -47,10 +48,10 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <input className={classes.Input} type='text' name='name' placeholder='Name' />
-                <input className={classes.Input} type='text' name='email' placeholder='Email' />
-                <input className={classes.Input} type='text' name='street' placeholder='Street' />
-                <input className={classes.Input} type='text' name='postalCode' placeholder='Postal Code' />
+                <Input inputtype='input' type='text' name='name' placeholder='Name' />
+                <Input inputtype='input' type='text' name='email' placeholder='Email' />
+                <Input inputtype='input' type='text' name='street' placeholder='Street' />
+                <Input inputtype='input' type='text' name='postalCode' placeholder='Postal Code' />
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
