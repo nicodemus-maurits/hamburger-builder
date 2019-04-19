723b1012b981db527a3d8bad3063feb78a50a285
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-04-19 22:35:17 +0200

    Create action creators to store orderData

---
+++ src/store/actions/order.js
@@ -0,0 +1,31 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+/* Synchronous action */
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error: error
+    };
+};
+
+/* Asynchronous action */
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json', orderData)
+            .then(response => {
+                dispatch(purchaseBurgerSuccess(response.data, orderData));
+            })
+            .catch(error => {
+                dispatch(purchaseBurgerFail(error));
+            });
+    };
+};
