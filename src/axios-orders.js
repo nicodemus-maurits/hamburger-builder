3c829b9d3435415402228a3904d8668f4f136b9b
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-04-21 15:39:34 +0200

    Comment out the auth variable since user are authenticated with token

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -4,7 +4,7 @@ const instance = Axios.create({
     baseURL: 'https://react-burgerbuilder-3f0bd.firebaseio.com/'
 });
 
-Axios.defaults.params = {}
-Axios.defaults.params['auth'] = process.env.REACT_APP_AUTH_KEY;
+/* Axios.defaults.params = {}
+Axios.defaults.params['auth'] = process.env.REACT_APP_AUTH_KEY; */
 
 export default instance;
