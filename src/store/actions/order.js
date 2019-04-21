68a3256a7f7c99ab393fdb33bc432eb008af419d
Maurits Nicodemus <nicodemus.maurits@gmail.com>
2019-04-21 15:49:56 +0200

    Limit order feature to logged in user only

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -24,10 +24,10 @@ export const purchaseBurgerStart = () => {
 };
 
 /* Asynchronous action */
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
-        axios.post('/orders.json', orderData)
+        axios.post('/orders.json?auth=' + token, orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData));
             })
